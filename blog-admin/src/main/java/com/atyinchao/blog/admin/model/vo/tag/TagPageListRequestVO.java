d7ea2ad40be5cfeba29a790c1b30372c918fae53
yinchao <11091176+yc_3219815800@user.noreply.gitee.com>
2024-12-13 17:57:38 +0800

    标签功能开发

---
+++ blog-admin/src/main/java/com/atyinchao/blog/admin/model/vo/tag/TagPageListRequestVO.java
@@ -0,0 +1,28 @@
+package com.atyinchao.blog.admin.model.vo.tag;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.checkerframework.checker.units.qual.A;
+
+import java.util.Date;
+
+/**
+ * @ClassName TagPageListRequestVO
+ * @Description
+ * @Date 2024/12/13 15:00
+ * @Author yinchao
+ **/
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TagPageListRequestVO {
+
+    private String name;
+
+    private Date startTime;
+
+    private Date endTime;
+}
